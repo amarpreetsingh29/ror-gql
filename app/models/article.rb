f93ee7565349c3065be9870e1a13269092a9330c
Amarpreet Singh <amarpreet.talwar@gmail.com>
2023-05-09 10:15:07 -0700

    pluralize has_and_belongs_to_many associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
 class Article < ApplicationRecord
-  has_and_belongs_to_many :author, join_table: 'article_and_authors'
+  has_and_belongs_to_many :authors, join_table: 'article_and_authors'
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   has_many :book_and_authors
   has_many :author, through: :book_and_authors
-  has_and_belongs_to_many :order
+  has_and_belongs_to_many :orders
 end

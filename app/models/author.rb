8ee336b188eb3b26597749abbd7ac8cf0052a7fe
Amarpreet Singh <amarpreet.talwar@gmail.com>
2023-05-10 01:30:53 -0700

    modify book author association

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ApplicationRecord
   has_and_belongs_to_many :articles, join_table: 'article_and_authors'
-  has_many :book_and_authors
-  has_many :book, through: :book_and_authors
+  # has_many :book_and_authors
+  # has_many :book, through: :book_and_authors
   has_one :account
+  has_and_belongs_to_many :books, join_table: 'book_and_authors'
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
-  has_many :book_and_authors
-  has_many :author, through: :book_and_authors
+  # has_many :book_and_authors
+  # has_many :author, through: :book_and_authors
   has_and_belongs_to_many :orders
+  has_and_belongs_to_many :authors, join_table: 'book_and_authors'
 end

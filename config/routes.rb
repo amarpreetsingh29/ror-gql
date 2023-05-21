bab807eab641c6703976e095e8c4ca577e9f8d53
Amarpreet Singh <amarpreet.talwar@gmail.com>
2023-05-20 19:42:18 -0700

    add show articleby id route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  get "/article/:id", to: "articles#show"
 end

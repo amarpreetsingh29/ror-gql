bcccbd0405988281cab3d6f313cfa01c0fcf05cc
Amarpreet Singh <amarpreet.talwar@gmail.com>
2023-05-20 19:39:29 -0700

    show article with id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,8 @@ class ArticlesController < ApplicationController
       @articles = Article.all
    end
 
-   def show(id)
-      @article = Article.find(params[:id])
+   def show
+      id = params[:id]
+      @article = Article.where(id: params[:id]).first
    end
 end
